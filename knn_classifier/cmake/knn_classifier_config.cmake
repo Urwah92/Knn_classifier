2ef3c912e586fc7d42d3f66fe4fb5bf1a3dbe9d9
Urwah92 <76911824+Urwah92@users.noreply.github.com>
2022-05-11 18:28:48 +0200

    Knn_classifier
    Package

---
+++ knn_classifier/cmake/knn_classifier_config.cmake
@@ -0,0 +1,6 @@
+set(knn_classifier_NAMESPACE           knn_classifier)
+set(knn_classifier_TYPESYSTEM_CPP_PATH /home/rs2004/rs_ws/src/knn_classifier/include/knn_classifier/types)
+set(knn_classifier_TYPESYSTEM_XML_PATH /home/rs2004/rs_ws/src/knn_classifier/descriptors/typesystem)
+set(knn_classifier_ANNOTATOR_PATH      /home/rs2004/rs_ws/src/knn_classifier/descriptors/annotators)
+set(knn_classifier_ENGINE_PATH         /home/rs2004/rs_ws/src/knn_classifier/descriptors/analysis_engines)
+
